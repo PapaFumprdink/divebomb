772fb55ac5357505d74f70e10a4ee3ab499773a7
PapaFumprdink <Boschdog03@gmail.com>
2021-07-23 17:39:19 +1000

    I dont know tbh

--- Assets/Scripts/Cloud.cs
+++ Assets/Scripts/Cloud.cs
@@ -7,25 +7,46 @@ using UnityEngine;
 public sealed class Cloud : MonoBehaviour
 {
     [SerializeField] private ParticleSystem m_ImpactFX;
+    [SerializeField] private AnimationCurve m_ImpactFXScale;
+    [SerializeField] private float m_SizeVariation;
+    [SerializeField] private AnimationCurve m_ImpactFXSpeed;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        // If something enters the cloud, display the collision fx.
-        m_ImpactFX.transform.position = collision.transform.position;
-        m_ImpactFX.transform.rotation = collision.attachedRigidbody
-            ? Quaternion.LookRotation(Vector3.forward, collision.attachedRigidbody.velocity) * Quaternion.Euler(0f, 0f, 180f)
-            : collision.transform.rotation * Quaternion.Euler(0f, 0f, 180f);
-        m_ImpactFX.Play();
+        ShowImpactFX(collision, true);
     }
 
     private void OnTriggerExit2D(Collider2D collision)
     {
-        // If something exits the cloud, display the collision fx.
-        m_ImpactFX.transform.position = collision.transform.position;
-        m_ImpactFX.transform.rotation = collision.attachedRigidbody
-            ? Quaternion.LookRotation(Vector3.forward, collision.attachedRigidbody.velocity)
-            : collision.transform.rotation * Quaternion.Euler(0f, 0f, 180f);
+        ShowImpactFX(collision, false);
+    }
 
-        m_ImpactFX.Play();
+    private void ShowImpactFX(Collider2D collision, bool flip)
+    {
+        // If something exits the cloud, display the collision fx.
+        Quaternion effectRotation = collision.attachedRigidbody
+                    ? Quaternion.LookRotation(Vector3.forward, collision.attachedRigidbody.velocity) * Quaternion.Euler(0f, 0f, flip ? 180f : 0f)
+                    : collision.transform.rotation * Quaternion.Euler(0f, 0f, 180f);
+
+        ParticleSystem newFX = Instantiate(m_ImpactFX, collision.transform.position, effectRotation);
+        if (collision.attachedRigidbody)
+        {
+            ParticleSystem.MainModule main = newFX.main;
+            float particleScale = m_ImpactFXScale.Evaluate(collision.attachedRigidbody.velocity.magnitude);
+            main.startSize = new ParticleSystem.MinMaxCurve
+            {
+                constantMin = particleScale - m_SizeVariation * particleScale,
+                constantMax = particleScale + m_SizeVariation * particleScale,
+                mode = ParticleSystemCurveMode.TwoConstants,
+            };
+            main.startSpeed = new ParticleSystem.MinMaxCurve
+            {
+                constantMin = 0,
+                constantMax = m_ImpactFXSpeed.Evaluate(collision.attachedRigidbody.velocity.magnitude),
+                mode = ParticleSystemCurveMode.TwoConstants,
+            };
+        }
+
+        newFX.Play();
     }
 }
